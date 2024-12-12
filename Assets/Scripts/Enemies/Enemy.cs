f068a072c1d2ee69beb2d3b3ede7a8e4d22ba648
Wbeimar Agudelo <andrey.agudelo12@gmail.com>
2024-12-12 17:06:02 -0500

    Background

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -44,7 +44,7 @@ public class Enemy : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.transform.CompareTag("Shield"))
+        if (collision.collider.CompareTag("Shield"))
         {
             //Decrease life
             TakeDamage();
